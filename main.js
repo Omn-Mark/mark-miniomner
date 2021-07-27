1efcca45830a990b87ba34e75ae69a14ac2b4c18
Omn-Mark <86652155+Omn-Mark@users.noreply.github.com>
2021-07-27 16:26:21 +0900

    add board.html

--- main.js
+++ main.js
@@ -1,9 +1,26 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 8080
+const mysql = require('mysql');
+
+app.use(express.urlencoded({extended : true}));
+
+
+const connection = mysql.createConnection({
+    host : 'us-cdbr-east-04.cleardb.com',
+    user : 'b7e214b16f41fe',
+    password : '2ec4b087',
+    database : 'heroku_0fa42acffb3e5bc'
+})
+
+connection.connect((err) => {
+    if (err) throw err;
+    console.log("connect!");
+
+})
 
 app.get('/', (req, res) => {
-    res.send('Hello');
+    res.sendFile(__dirname + '/board/board.html');
 });
 
 app.listen(port, () => {
