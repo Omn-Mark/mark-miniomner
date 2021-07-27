903c3ca794817c599c72c9e98e6d427cab747b69
Omn-Mark <86652155+Omn-Mark@users.noreply.github.com>
2021-07-27 15:36:04 +0900

    change port number

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = process.env.port || 5000
+const port = process.env.port || 8080
 
 app.get('/', (req, res) => {
     res.send('Hello');
