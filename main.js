1dfec64db0c35049ecea5b0d8e1d7686bf3c44a2
Omn-Mark <86652155+Omn-Mark@users.noreply.github.com>
2021-07-27 14:59:53 +0900

    modified port

--- main.js
+++ main.js
@@ -6,7 +6,4 @@ app.get('/', (req, res) => {
     res.send('Hello');
 });
 
-app.listen(port,(err) => {
-    if(err) return console.log(err);
-    console.log('the server is listening on port 5000');
-});
+app.listen(port);
